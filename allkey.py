11cfb468ae557af927b9b8995e081386eaefe8b0
guantou <mole30608@gmail.com>
2022-12-16 02:09:44 +0800

    Create allkey.py

---
+++ allkey.py
@@ -0,0 +1,33 @@
+#press anykey will play the song
+
+from  pynput import keyboard
+from playsound import playsound
+import multiprocessing
+import os,time
+
+def playsong():
+    playsound(sound=os.getcwd()+"\\song.wav") # song's file format must be wav
+
+p=multiprocessing.Process(target=playsong)
+
+def on_press(key):
+    global p
+    if p.is_alive():
+        p.terminate()
+        p = multiprocessing.Process(target=playsong)
+        p.start()
+    else:
+        p = multiprocessing.Process(target=playsong)
+        p.start()
+   
+
+def on_release(key):
+    pass
+    # if key == keyboard.Key.esc: # press esc to stop
+    #     return False
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
+        listener.join()
+        
