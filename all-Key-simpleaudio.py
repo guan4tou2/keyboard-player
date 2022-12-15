cec2da87cb00bd234cb21d02ef7e21bb0cb4e793
guantou <mole30608@gmail.com>
2022-12-16 02:55:12 +0800

    Create all-Key-simpleaudio.py

---
+++ all-Key-simpleaudio.py
@@ -0,0 +1,26 @@
+#press anykey will play the song
+
+from pynput import keyboard
+import os
+import simpleaudio as sa
+
+wave_obj = sa.WaveObject.from_wave_file(os.getcwd()+"\\song.wav")
+play_obj = wave_obj.play()
+play_obj.stop()
+
+def on_press(key):
+    global play_obj
+    if play_obj.is_playing():
+        play_obj.stop()
+        play_obj = wave_obj.play()
+    else:
+        play_obj = wave_obj.play()
+
+def on_release(key):
+    pass
+    # if key == keyboard.Key.esc: # press esc to stop
+    #     return False
+
+if __name__ == '__main__':
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
